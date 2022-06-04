495da9bdf911146026a3f0950f07f8bcaf8f3cb0
Koala <70296933+m-yassine-kun@users.noreply.github.com>
2022-06-04 23:53:46 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,24 @@
+<script type="module">
+  
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDR39JfdjibhaGLDIFyCTW3oJc23K1kMiY",
+    authDomain: "contact-form-portfolio-12345.firebaseapp.com",
+    projectId: "contact-form-portfolio-12345",
+    storageBucket: "contact-form-portfolio-12345.appspot.com",
+    messagingSenderId: "797760920832",
+    appId: "1:797760920832:web:4b865779fcbdc91df9e7e5"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
+
 /*!
-* Start Bootstrap - Freelancer v7.0.6 (https://startbootstrap.com/theme/freelancer)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
-*/
+
 //
 // Scripts
 // 
