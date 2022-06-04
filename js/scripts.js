fc5bc4e84a28ac8805f8092d85c7fc9a06fc4f1a
Koala <70296933+m-yassine-kun@users.noreply.github.com>
2022-06-05 00:15:42 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-<script type="module">
+
   
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
@@ -15,7 +15,53 @@
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-</script>
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+  e.preventDefault();
+
+  // Get values
+  var name = getInputVal('name');
+  var email = getInputVal('email');
+  var phone = getInputVal('phone');
+  var message = getInputVal('message');
+
+  // Save message
+  saveMessage(name, email, phone, message);
+
+  // Show alert
+  //document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  //setTimeout(function(){
+    //document.querySelector('.alert').style.display = 'none';
+  //},3000);
+
+  // Clear form
+  document.getElementById('contactForm').reset();
+}
+
+// Function to get get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, email, phone, message){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    email:email,
+    phone:phone,
+    message:message
+  });
+}
 
 /*!
 
